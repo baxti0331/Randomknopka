83249b01a655636aa771c239a4ae037e8d30ea76
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-06-22 23:26:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
+import os
+import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
-import asyncio
-import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Получим токен из переменной окружения
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEB_APP_URL = https://random-wine.vercel.app/
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
-async def handle_start(message: types.Message):
+async def start_handler(message: types.Message):
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="PLAY 🕹️", web_app=WebAppInfo(url=WEB_APP_URL))]
+            [
+                InlineKeyboardButton(
+                    text="PLAY 🕹️",
+                    web_app=WebAppInfo(url=WEB_APP_URL)
+                )
+            ]
         ]
     )
-    await message.answer("Нажмите кнопку ниже, чтобы запустить мини-приложение 🎲", reply_markup=keyboard)
+    await message.answer(
+        "Нажмите кнопку ниже, чтобы запустить мини-приложение 🎲",
+        reply_markup=keyboard
+    )
 
 async def main():
     await dp.start_polling(bot)
